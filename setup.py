298a3d134b37911aea0498096ddd9a545f1c6c82
Oleksandr Yanov <oleksandr.yanov@gmail.com>
2022-01-09 12:32:27 +0200

    Chnaged version to 0.0.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name='lib_example',
-    version='0.0.1',
+    version='0.0.2',
     author='Horsa Gorbunchul',
     author_email='hb@gmail.com',
     description='Test lib to check how to include private libs',
