2af8a571bb9551ec07235ac45adac8752fcfd95b
Oleksandr Yanov <oleksandr.yanov@gmail.com>
2022-01-09 13:52:51 +0200

    Added new function 3 in version 0.0.6

--- lib_example/functions.py
+++ lib_example/functions.py
@@ -8,3 +8,6 @@ def new_func2():
     print("You see new func2!")
 
 
+def new_func3():
+    print("You see new func3!")
+
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name='lib_example',
-    version='0.0.5',
+    version='0.0.6',
     author='Horsa Gorbunchul',
     author_email='hb@gmail.com',
     description='Test lib to check how to include private libs',
