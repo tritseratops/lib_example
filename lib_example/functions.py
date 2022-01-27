8f37336b515daca81c7385850ae79439fdf16732
Oleksandr Yanov <oleksandr.yanov@gmail.com>
2022-01-27 10:28:07 +0200

    Added pin functions and gitignore

--- lib_example/functions.py
+++ lib_example/functions.py
@@ -1,27 +1,6 @@
-def super_func():
-    print("I used super func in my library")
+def pin_tests():
+    print("Executing pin tests")
 
-def new_func():
-    print("You see new func!")
+def general_tests():
+    print("Executing general tests")
 
-def new_func2():
-    print("You see new func2!")
-
-
-def new_func3():
-    print("You see new func3!")
-
-def new_func4():
-    print("You see new func4!")
-
-def new_func6():
-    print("You see new func6!")
-
-def new_func7():
-    print("You see new func7!")
-
-def new_func8():
-    print("You see new func8!")
-
-def new_func9():
-    print("You see new func9!")
